0fb287bbdf3319f3e825dbb94e09c9754b0c478f
Thijs Haker <theage2001@gmail.com>
2022-12-04 20:23:23 +0100

    Added find and replace functions

---
+++ ops.go
@@ -0,0 +1,74 @@
+package lib1n
+
+// Return new dataset, where old dataset matched
+func Find(ds DataSet, pattern string) DataSet {
+	type result struct {
+		key string
+		val []string
+	}
+
+	var chResult = make(chan result, len(ds))
+
+	// For each key/value pair, try concurrently
+	// to match the pattern
+	for key, val := range ds {
+		go func(pattern string, key string, vals []string) {
+			for _, val := range vals {
+				// If pattern matched:
+				// Send trough channel, and return
+				if val == pattern {
+					chResult <- result{key: key, val: vals}
+					return
+				}
+			}
+			// If no pattern matched, send empty
+			chResult <- result{key: "", val: nil}
+		}(pattern, key, val)
+	}
+
+	nds := make(DataSet, len(ds))
+
+	// Collect results, and return new dataset
+	for i := 0; i < len(ds); i++ {
+		res := <-chResult
+		if res.val != nil {
+			nds[res.key] = res.val
+		}
+	}
+
+	return nds
+}
+
+// Return new dataset, where old values are replaced
+func Replace(ds DataSet, pattern string, replace string) DataSet {
+	type result struct {
+		key string
+		val []string
+	}
+
+	var chResult = make(chan result, len(ds))
+
+	// For each key/value pair, try concurrently
+	// to match and replace pattern
+	for key, val := range ds {
+		go func(pattern string, replace string, key string, vals []string) {
+			for i, val := range vals {
+				// If pattern matched, replace it
+				if val == pattern {
+					vals[i] = replace
+				}
+			}
+			chResult <- result{key: key, val: vals}
+		}(pattern, replace, key, val)
+	}
+
+	nds := make(DataSet, len(ds))
+
+	// Collect results, and return new dataset
+	for i := 0; i < len(ds); i++ {
+		res := <-chResult
+		nds[res.key] = res.val
+	}
+
+	return nds
+}
---
+++ ops_test.go
@@ -0,0 +1,45 @@
+package lib1n
+
+import "testing"
+
+func TestFind(t *testing.T) {
+	ds := DataSet{
+		"0": {"X", "O", "O"},
+		"1": {"O", "O", "O"},
+		"2": {"O", "O", "X"},
+	}
+
+	nds := Find(ds, "X")
+
+	_, check0 := nds["0"]
+	_, check1 := nds["1"]
+	_, check2 := nds["2"]
+
+	if check0 && !check1 && check2 {
+		t.Log("Pattern found at the right indeces")
+		return
+	} else {
+		t.Fail()
+	}
+}
+
+func TestReplace(t *testing.T) {
+	ds := DataSet{
+		"0": {"X", "O", "O"},
+		"1": {"O", "X", "O"},
+		"2": {"O", "O", "X"},
+	}
+
+	nds := Replace(ds, "X", "T")
+
+	check0 := nds["0"][0] == "T"
+	check1 := nds["1"][1] == "T"
+	check2 := nds["2"][2] == "T"
+
+	if check0 && check1 && check2 {
+		t.Log("Pattern found, and replaced at the right indeces")
+		return
+	} else {
+		t.Fail()
+	}
+}
